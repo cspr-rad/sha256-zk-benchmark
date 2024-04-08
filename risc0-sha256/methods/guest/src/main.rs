a2c00c9cbcae9db28d8824a9a096cdc3e19b579a
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-04-08 13:51:35 +0200

    groth16 feature for risc0

--- risc0-sha256/methods/guest/src/main.rs
+++ risc0-sha256/methods/guest/src/main.rs
@@ -1,21 +1,29 @@
 #![no_main]
+#![no_std]
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std]  // std support is experimental
-
-
 use risc0_zkvm::guest::env;
-
+use risc0_zkvm::sha::{Digest, Impl, Sha256};
+extern crate alloc;
+use alloc::vec::Vec;
 risc0_zkvm::guest::entry!(main);
-
+use serde::{Serialize, Deserialize};
+#[derive(Serialize, Deserialize)]
+struct Output {
+    sha256_hashes: Vec<Digest>,
+}
 
 fn main() {
-    // TODO: Implement your guest code here
-
     // read the input
-    let input: u32 = env::read();
-
-    // TODO: do something with the input
-
-    // write public output to the journal
-    env::commit(&input);
+    // let input: u32 = env::read();
+    let mut output_hashes: Vec<Digest> = Vec::new();
+    // test sha256
+    for i in 0..1000 {
+        let sha_input = [1u8; 0];
+        let sha_hash: Digest = *Impl::hash_bytes(&sha_input);
+        output_hashes.push(sha_hash);
+    }
+    let output: Output = Output {
+        sha256_hashes: output_hashes,
+    };
+    env::commit(&output);
 }
